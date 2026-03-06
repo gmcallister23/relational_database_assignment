af02b057e4cd4999d18e45c6c72718e7ba46a44e
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-03-05 21:19:55 -0500

    added Base.metadata.drop_all(engine) to start the database over each time the program is run.  Cleaned up some of the testing code

--- relational_database_assignment.py
+++ relational_database_assignment.py
@@ -49,6 +49,7 @@ class Order(Base):
     product = relationship('Product', back_populates='orders')
     user = relationship('User', back_populates='orders')
 
+Base.metadata.drop_all(engine) #Drops the database so you can start over again, eachtime you run the code.  Comment this out if you don't want to drop the database everytime.
 Base.metadata.create_all(engine)
 
 session = Session()
@@ -61,30 +62,18 @@ product1 = Product(name='Ball', price = 50)
 product2 = Product(name='Claw', price = 150)
 product3 = Product(name='Widget', price = 100)
 
-old_user = session.query(User).filter_by(email='naenae@example.com').first()
-if old_user:
-    session.delete(old_user)
-    session.commit()
-    #session.expunge(old_user)
-
-
-# ##Un comment to add user back in, then re-comment out so you can delete them
-user1 = User(id=1, name='Lanae', email='naenae@example.com')
-session.add(user1)
+#Adds users and products to the database -->> comment out after initial commit so you don't duplicate --> Actually since, we have the drop_all at the top, we can leave this because the database resets each time, which stops the database from creating duplicates.
+session.add_all([user1, user2, product1, product2, product3]) #Must wrap all items in a bracket
 session.commit()
-
-#Adds users and products to the database -->> comment out after initial commit so you don't duplicate
-# session.add_all([user1, user2, product1, product2, product3]) #Must wrap all items in a bracket
-# session.commit()
 
 order1 = Order(user_id=1, product_id=1, quantity=2, is_shipped=False)
 order2 = Order(user_id=1, product_id=3, quantity=3, is_shipped=True)
 order3 = Order(user_id=2, product_id=2, quantity=4, is_shipped=True)
 order4 = Order(user_id=2, product_id=3, quantity=1, is_shipped=True)
 
-#Commented out so it doesn't run again and try to create a duplicate
-# session.add_all([order1, order2, order3, order4])
-# session.commit()
+#Commented out so it doesn't run again and try to create a duplicate --> Actually since, we have the drop_all at the top, we can leave this because the database resets each time, which stops the database from creating duplicates.
+session.add_all([order1, order2, order3, order4])
+session.commit()
 
 # user=session.query(User).filter_by(name='Lanae').first() #not necessary
 # product = session.query(Product).filter_by(name='Widget').first() #not necessary
@@ -127,14 +116,20 @@ for product in products:
     print(product.name, product.price)
 
 # # # #Deleting a user by id - this works for single use purposes
-# # user = session.query(User).filter_by(id=1).first()
+user = session.query(User).filter_by(id=1).first()
 
-# # if user:
-# #     session.delete(user)
-# #     session.commit()
-# #     print(f'User {user.name} deleted')
-# # else:
-# #     print('User not found')
+if user:
+    session.delete(user)
+    session.commit()
+    print(f'User {user.name} deleted')
+else:
+    print('User not found')
+
+# I was trying to figure out how to add a the use back in after I deleted them.  I tried a bunch of different ways,
+# but since the user email was linked to an order and because the table incremented each time, the user wouldn't ever
+# add back to user_id 1.  So we can still delet, but I added the drop_all at the top to set the code.  Some of the comments
+# are from trying to figure out how to add the user back in after deletion, but I couldn't get it to work because of the 
+# relationships and the fact that the id's increment each time.  So I just added the drop_all at the top to reset the database each time.
 
 # #Delete the 'old' user to get rid of the email
 
@@ -149,3 +144,6 @@ for product in products:
 # # user1 = User(name='Lanae', email='naenae@example.com')
 # # session.add(user1)
 # # session.commit()
+
+
+
